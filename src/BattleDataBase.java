b063e3edbcd2cbab6ff6ddda066a57baf9872587
jramzz1 <jesus.g.ramirez01@utrgv.edu>
2018-04-27 13:22:23 -0500

    changes

--- src/BattleDataBase.java
+++ src/BattleDataBase.java
@@ -8,43 +8,71 @@ import java.util.Scanner;
 import com.mysql.jdbc.PreparedStatement;
 
 public class BattleDataBase {
+	
+	public Object[][] getTable0(String userName, String pass, String wins, String loses, String average) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection connection = DriverManager.getConnection("jdbc:mysql://35.226.99.168:3306/battleship", "root", "1234qwer");
+		Statement statement = connection.createStatement();
+		
+		String update = "select * from BSScores ORDER BY KD DESC";
+		ResultSet rs = statement.executeQuery(update);
+		
+		Object[][] data = new Object[100][8];
+		int count = 0;
+		
+		while(rs.next()) {
+			data[count][0] = rs.getString(1);
+			data[count][1] = rs.getString(3);
+			data[count][2] = rs.getString(4);
+			data[count][3] = rs.getString(5);
+			++count;
+		}
+		connection.close();
+		return data;
+	}
 //
 //	public static void main(String[] args) {
 //		
 //	}
 	
 //		READY TO MANIPULATE
-	public static void Display() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
-	{	
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		Connection connection = DriverManager.getConnection("jdbc:mysql://35.226.99.168:3306/battleship", "root", "1234qwer");
-		Statement statement = connection.createStatement();
-		ResultSet rs = statement.executeQuery("select * from BSScores");
-		System.out.println("ID" + "            "
-				+ "Password" + "       "
-				+ "Wins" + "     "
-				+ "Loses" + "     "
-				+ "W/L Ratio");
+//	public void Display() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+//	{	
+//		Class.forName("com.mysql.jdbc.Driver").newInstance();
+//		Connection connection = DriverManager.getConnection("jdbc:mysql://35.226.99.168:3306/battleship", "root", "1234qwer");
+//		Statement statement = connection.createStatement();
+//		ResultSet rs = statement.executeQuery("select * from BSScores");
+//		System.out.println("ID" + "            "
+//				+ "Password" + "       "
+//				+ "Wins" + "     "
+//				+ "Loses" + "     "
+//				+ "W/L Ratio");
+//		
+//		String uID ="", uPW = "";
+//		int W = 0, L = 0;
+//		float WL = 0;
+//		while(rs.next())
+//		{	
+//			uID = rs.getString("userID");
+//			uPW = rs.getString("userPW");
+//			W = rs.getInt("wins");
+//			L = rs.getInt("loses");
+//			WL = rs.getFloat("KD");
+//			
+//			
+//			System.out.printf("%8s %10s %10s %10s %10s", uID, uPW, W, L, WL);
+//			System.out.println();
+//		}
+//		connection.close();
+//	}
+	
+	public static void updateW() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+	{
 		
-		String uID ="", uPW = "";
-		int W = 0, L = 0;
-		float WL = 0;
-		while(rs.next())
-		{	
-			uID = rs.getString("userID");
-			uPW = rs.getString("userPW");
-			W = rs.getInt("wins");
-			L = rs.getInt("loses");
-			WL = rs.getFloat("KD");
-			
-			
-			System.out.printf("%8s %10s %10s %10s %10s", uID, uPW, W, L, WL);
-			System.out.println();
-		}
-		connection.close();
 	}
 	
-	public static boolean logIn(String ID, String Pass) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+	public static boolean logIn(String ID, String Pass, int a) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
 	{	
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		Connection connection = DriverManager.getConnection("jdbc:mysql://35.226.99.168:3306/battleship", "root", "1234qwer");
@@ -56,6 +84,8 @@ public class BattleDataBase {
 			if( ID.equals(rs.getString("userID")) && Pass.equals(rs.getString("userPW")))
 			{
 				verified = true;
+				String str = "UPDATE BSScores SET LoggedIN = '" + a + "' WHERE userID = '" + ID + "'";
+				statement.executeUpdate(str);
 				break;
 			}
 		}
@@ -88,17 +118,8 @@ public class BattleDataBase {
 		Connection connection = DriverManager.getConnection("jdbc:mysql://35.226.99.168:3306/battleship", "root", "1234qwer");
 		Statement statement = connection.createStatement();
 		
-//		INSERT INTO BSScores VALUES ('rigosans', 'passw', 10, 0, 10);
-		int W = 0, L = 0;
-		float KD = 0;
-		String str = "INSERT INTO BSScore (userID, UserPW, wins, loses, KD)" + "VALUES(?,?,?,?,?)";
-		PreparedStatement ps = (PreparedStatement) connection.prepareStatement(str);
-		ps.setString(1, ID);
-		ps.setString(2, Pass);
-		ps.setInt(3, W);
-		ps.setInt(4, L);
-		ps.setFloat(5, KD);
-		ps.executeUpdate();
+		String str = "INSERT INTO BSScores (userID, UserPW, wins, loses, KD, LoggedIN)" + "VALUES('" + ID + "','" + Pass + "','0','0','0','0')";
+		statement.executeUpdate(str);
 		connection.close();
 	}
 	
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -8,7 +8,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,14 +20,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class MainMenu{
+public class MainMenu extends JFrame{
 	
 	private JTextField usr1, usr2, pswd1, pswd2;
-	private JLabel user1, user2,pass1,pass2;
-	private JFrame window;
+	private JLabel user1, user2, pass1, pass2, user, hscore, avg;
+	private JFrame window,scorewindow;
 	private ImageIcon backgroundImageIcon;
 	private JLabel bkgImageContainer;
 	private JButton startGame;
+	
+	private JTable table;
+	private DefaultTableModel dm;
+	private Object[][] data = {{"","",""}};
+	private JTextField tf1, tf2, tf3, tf4, tf5;
+	private String[] columnNames = {"Username", "Wins", "Loses", "Average"};
+	private JScrollPane scroll;
+	private JButton scores;
+	
+	BattleDataBase ci = new BattleDataBase();
 
 	private volatile boolean isImageVisible;
 
@@ -32,11 +45,14 @@ public class MainMenu{
 		window = theWindow;
 		backgroundImageIcon = new ImageIcon("Title.png");
 		bkgImageContainer = new JLabel(backgroundImageIcon);
+		window.setLocation(280, bkgImageContainer.getHeight() + 150);
 		isImageVisible = true;
 	}
 	
+	public void ScoreWindow(JFrame scoreWindow) {
+		scorewindow = scoreWindow;
+	}
 
-	
 	public void loadTitleScreen() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -72,7 +88,6 @@ public class MainMenu{
 		pswd1.setLocation(222,375);
 		window.add(pswd1);
 		
-		
 		user2 = new JLabel("Player 2: Username");
 		user2.setSize(150,30);
 		user2.setLocation(482,290);
@@ -100,10 +115,95 @@ public class MainMenu{
 		String player2ID = usr2.getText();
 		String player2PW = pswd2.getText();
 		
+		scores = new JButton("Scores");
+		scores.setSize(100, 40);
+		scores.setLocation(750, bkgImageContainer.getHeight() + 220);// 150);
+		scores.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				setSize(600,400);
+				setLocation(430, bkgImageContainer.getHeight() - 50);
+				setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				setLayout(null);
+				
+				table = new JTable();
+				dm = new DefaultTableModel(data,columnNames);
+				table.setModel(dm);
+				scroll = new JScrollPane(table); 
+				scroll.setSize(500, 270); 
+				scroll.setLocation(50, 50); 
+				add(scroll);
+				
+				tf1 = new JTextField();
+				tf1.setSize(100,30);
+				tf1.setLocation(100,400);
+				add(tf1);
+				
+				tf2 = new JTextField();
+				tf2.setSize(100,30);
+				tf2.setLocation(100,400);
+				add(tf2);
+				
+				tf3 = new JTextField();
+				tf3.setSize(100,30);
+				tf3.setLocation(100,400);
+				add(tf3);
+				
+				tf4 = new JTextField();
+				tf4.setSize(100,30);
+				tf4.setLocation(100,400);
+				add(tf4);
+				
+				tf5 = new JTextField();
+				tf5.setSize(100,30);
+				tf5.setLocation(100,400);
+				add(tf5);
+				
+				try {
+					dm.setDataVector(ci.getTable0(tf1.getText(),tf2.getText(),tf3.getText(),tf4.getText(),tf5.getText()),columnNames);
+				} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				dm.fireTableDataChanged();
+				setVisible(true);
+				
+				
+//				window.getContentPane().remove(startGame);
+//				window.getContentPane().remove(scores);
+//				window.getContentPane().remove(bkgImageContainer);
+//				window.getContentPane().remove(user1);
+//				window.getContentPane().remove(usr1);
+//				window.getContentPane().remove(user2);
+//				window.getContentPane().remove(usr2);
+//				window.getContentPane().remove(pass1);
+//				window.getContentPane().remove(pswd1);
+//				window.getContentPane().remove(pass2);
+//				window.getContentPane().remove(pswd2);
+				
+				
+//				window.getContentPane().remove(gridSizeBtn);
+//				window.getContentPane().remove(battleshipSize);
+//				window.getContentPane().remove(cruiserSize);
+//				window.getContentPane().remove(destroyerSize);
+//				window.getContentPane().remove(submarineSize);
+//				window.getContentPane().remove(battleshipCount);
+//				window.getContentPane().remove(cruiserCount);
+//				window.getContentPane().remove(destroyerCount);
+//				window.getContentPane().remove(submarineCount);
+//				window.getContentPane().revalidate();
+//				window.getContentPane().repaint();
+//				window.getContentPane().setBackground(new Color(30,55,65));
+//				isImageVisible = false;
+			}	
+		});
+		
 		
 		//boolean player1B = false, player2B = false;
-		boolean player1B = BattleDataBase.logIn(player1ID, player1PW);
-		boolean player2B = BattleDataBase.logIn(player2ID, player2PW);
+		boolean player1B = BattleDataBase.logIn(player1ID, player1PW, 1);
+		boolean player2B = BattleDataBase.logIn(player2ID, player2PW, 2);
 		
 		startGame = new JButton("Start Game");
 		startGame.setSize(200, 80);
@@ -123,20 +223,20 @@ public class MainMenu{
 				//boolean player1B = false, player2B = false;
 				boolean player1B = false;
 				try {
-					player1B = BattleDataBase.logIn(player1ID, player1PW);
+					player1B = BattleDataBase.logIn(player1ID, player1PW, 1);
 				} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				boolean player2B = false;
 				try {
-					player2B = BattleDataBase.logIn(player2ID, player2PW);
+					player2B = BattleDataBase.logIn(player2ID, player2PW, 2);
 				} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
-				if(player1B == true && player2B == true)
+				if((player1B == true && player2B == true))
 				{
 					window.getContentPane().remove(startGame);
 					window.getContentPane().remove(bkgImageContainer);
@@ -148,13 +248,13 @@ public class MainMenu{
 					window.getContentPane().remove(pswd1);
 					window.getContentPane().remove(pass2);
 					window.getContentPane().remove(pswd2);
+					window.getContentPane().remove(scores);
 					window.getContentPane().revalidate();
 					window.getContentPane().repaint();
 					window.getContentPane().setBackground(new Color(30,55,65));
 					isImageVisible = false;
 				}
-//REMEMBER TO CREATE 2 NEW BUTTONS TO CREATE INDIVIDUAL USERS		
-	//2 BUTTONS!!!!!			
+			
 				else if(player1B == false && player2B == false)
 				{
 					Component frame = null;
@@ -162,6 +262,25 @@ public class MainMenu{
 						    "PLayer 1 & 2 not found! Create?",
 						    "Inane error",
 						    JOptionPane.ERROR_MESSAGE);
+					
+					try {
+						if((!player1ID.equals("") && !player1PW.equals("")) && (!player2ID.equals("") && !player2PW.equals("")))
+						{
+							boolean usr1 = BattleDataBase.userNameTaken(player1ID);
+							boolean usr2 = BattleDataBase.userNameTaken(player2ID);
+							
+							if(!usr1 || !usr2)
+							{
+								BattleDataBase.Create(player1ID, player1PW);
+								BattleDataBase.Create(player2ID, player2PW);
+							}
+						}
+					} catch (InstantiationException | IllegalAccessException | ClassNotFoundException
+							| SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 				}
 				else if(player1B == false)
 				{
@@ -170,6 +289,21 @@ public class MainMenu{
 						    "PLayer 1 not found! Create?",
 						    "Inane error",
 						    JOptionPane.ERROR_MESSAGE);
+					try {
+						if(!player1ID.equals("") && !player1PW.equals(""))
+						{
+							boolean usr1 = BattleDataBase.userNameTaken(player1ID);
+							
+							if(!usr1)
+							{
+								BattleDataBase.Create(player1ID, player1PW);
+							}
+						}
+					} catch (InstantiationException | IllegalAccessException | ClassNotFoundException
+							| SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 				else if(player2B == false)
 				{
@@ -178,6 +312,21 @@ public class MainMenu{
 						    "PLayer 2 not found! Create?",
 						    "Inane error",
 						    JOptionPane.ERROR_MESSAGE);
+					try {
+						if(!player2ID.equals("") && !player2PW.equals(""))
+						{
+							boolean usr2 = BattleDataBase.userNameTaken(player2ID);
+							
+							if(!usr2)
+							{
+								BattleDataBase.Create(player2ID, player2PW);
+							}
+						}
+					} catch (InstantiationException | IllegalAccessException | ClassNotFoundException
+							| SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+				}
 				}
 				
 //				window.getContentPane().remove(startGame);
@@ -202,6 +351,10 @@ public class MainMenu{
 		window.getContentPane().add(startGame);
 		window.getContentPane().add(bkgImageContainer);
 		window.getContentPane().setBackground(Color.BLACK);
+		
+		scores.setVisible(true);
+		window.getContentPane().add(scores);
+		
 		window.setVisible(true);
 		window.getContentPane().revalidate();
 		window.getContentPane().repaint();
